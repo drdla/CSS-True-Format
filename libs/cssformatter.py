58bfc4cdde00b2ee182e1f1f4ba46171235e9bff
drodler <drodler@laterpay.net>
2014-03-09 01:36:42 +0100

    a bit of clean-up

--- libs/cssformatter.py
+++ libs/cssformatter.py
@@ -13,14 +13,18 @@ import re
 def format_code(code):
     code = normalize_code(code)
     code = apply_LaterPay_style(code)
-    code = trim(code)
 
     return code
 
 def normalize_code(code):
-    code = re.sub(r'\s*([\{\}:;,])\s*', r'\1', code)                        # remove \s before and after characters {}:;,
-    code = re.sub(r'\n', r'', code)                                         # remove \n
+    code = code.strip()
     code = re.sub(r'\t', '    ', code)                                      # replace tabs with four spaces
+    code = re.sub(r'[\r\f\v]', '\n', code)                                  # replace whitespace characters other than space with \n
+    code = re.sub(r'\s*([\{:;,])\s*', r'\1', code)                          # remove \s before and after {:;,
+    code = re.sub(r'\s*(\})', r'\1', code)                                  # remove \s before }
+    code = re.sub(r'\n[ ]+\n', r'\n\n', code)                               # remove spaces between \n
+    code = re.sub(r'(\})\n', r'\1', code)                                   # remove \n after }
+
     code = re.sub(r',[\d\s\.\#\+>:]*\{', '{', code)                         # remove invalid selectors
     code = re.sub(r';\s*;', ';', code)                                      # remove superfluous ;
     code = re.sub(r'\/\*\s*([\s\S]+?)\s*\*\/', r'/* \1 */', code)           # add space before and after comment content
@@ -38,25 +42,28 @@ def apply_LaterPay_style(code):
     code = re.sub(r'{(\s)(\S)', r'{\2', code)                               # remove space after {
     code = re.sub(r'(\s)\}', r'\1}', code)                                  # remove space before }
     code = re.sub(r'([^;])\}', r'\1;}', code)                               # add missing ; before }
-    code = re.sub(r'\}\s*', r'}\n', code)                                   # add \n after }
+    code = re.sub(r'\}', r'}\n', code)                                      # add \n after }
+    code = re.sub(r'\n{3,}', r'\n\n\n', code)                               # preserve up to two consecutive empty lines
 
     code = re.sub(r'\s*([\:])\s*', r'\1', code)                             # remove space after :
     code = re.sub(r'(\S);([^\}])', r'\1; \2', code)                         # add space after ; except for before }
-    # TODO: add space around > + /
+    code = code.replace('"', '\'')                                          # replace " with '
+    code = re.sub(r'(\S)([>+])', r'\1 \2', code)                            # add space before > +
+    code = re.sub(r'([>+])(\S)', r'\1 \2', code)                            # add space after > +
+    code = re.sub(r':0\.', r':.', code)                                     # remove 0 from 0.x values directly after :
     code = comma_rules(code)                                                # add space or \n after ,
 
     code = re.sub(r'\;\s*(\/\*[^\n]*\*\/)\s*', r'; \1\n', code)             # fix comment after ;
     code = re.sub(r'(\/\*[^\n]*\*\/)\s+\}', r'\1}', code)                   # remove \n between comment and }
 
-    # TODO: preserve (up to two) empty lines
-    # TODO: add space around *, if at least one adjacent character is a number (in order to preserve hacks)
-    # TODO: remove " or ' with url(...)
-    # TODO: compress hex color values
+    # FIXME: don´t break data URI with rules for , and ; -> data:image/png;base64,iVBORw0...
 
     code = fix_0_values(code)
     code = sort_properties(code)
     code = expand_long_rules(code)
     code = indent_rules(code)
+
+    code = code.strip() + '\n'                                              # make sure the file ends with a newline
 
     return code
 
@@ -87,7 +94,7 @@ def sort_properties(code):
 
     for i in range(len(lines)):
         # get CSS rule by trimming current line
-        rule = trim(lines[i])
+        rule = lines[i].strip()
 
         # extract CSS selector from rule
         selectorEnd = rule.find('{')
@@ -103,6 +110,8 @@ def sort_properties(code):
             properties = filter(None, properties)
             properties = sorted(properties)
 
+            properties = [prop.lower() for prop in properties]              # lowercase all properties and their values
+
             rule = selector + '{' + '; '.join(properties) + ';}'            # reconstruct rule
 
         lines[i] = rule
@@ -112,7 +121,7 @@ def sort_properties(code):
     return code
 
 def expand_long_rules(code):
-    expand_threshold = 100                                                  # expand rules that exceed a given line length
+    expand_threshold = 120                                                  # expand rules that exceed a given line length
     lines = code.split('\n')
 
     for i in range(len(lines)):
@@ -135,20 +144,15 @@ def indent_rules(code):
         increment = lines[i].count('{') - lines[i].count('}')
         level = level + increment
         thisLevel = level - increment if increment > 0 else level
-        lines[i] = trim(lines[i])
+        lines[i] = lines[i].strip()
         lines[i] = spaces * thisLevel + lines[i]
 
     code = '\n'.join(lines)
 
     return code
 
-def trim(code):
-    code = re.sub(r'^\s*(\S+(\s+\S+)*)\s*$', r'\1', code)
-
-    return code
-
 def fix_0_values(code):
-    code = re.sub(r'\s0[emprtx%]+', r' 0', code)                            # remove unit from 0 values after \s
-    code = re.sub(r':0[emprtx%]+', r':0', code)                             # remove unit from 0 values after :
+    code = re.sub(r'\s0[emprx%]+', r' 0', code)                             # remove unit from 0 values after \s
+    code = re.sub(r':0[emprx%]+', r':0', code)                              # remove unit from 0 values after :
 
     return code
