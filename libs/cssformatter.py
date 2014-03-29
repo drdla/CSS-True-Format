1af170455f4349e2dd846e497c0927fb4ece1911
drodler <drodler@laterpay.net>
2014-03-29 22:37:19 +0100

    Refactored everything into own methods

--- libs/cssformatter.py
+++ libs/cssformatter.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 #
 # Format CSS/SASS/SCSS/LESS code according to LaterPay styleguide.
-#   written by Dominik Rodler <drodler@laterpay.net>
+# Written by Dominik Rodler <drodler@laterpay.net>
 #
 # usage:
 #   format_code(code)
@@ -24,92 +24,164 @@ def format_code(code):
 
 
 def normalize_code(code):
-    code = code.strip()
-    # replace tabs with four spaces
-    code = re.sub(r'\t', '    ', code)
-    # replace whitespace characters other than space with \n
-    code = re.sub(r'[\r\f\v]', '\n', code)
-    # remove \s before and after {:;,
-    code = re.sub(r'\s*([\{:;,])\s*', r'\1', code)
-    # remove \s before }
-    code = re.sub(r'\s*(\})', r'\1', code)
-    # remove spaces between \n
-    code = re.sub(r'\n[ ]+\n', r'\n\n', code)
-    # remove \n after }
-    code = re.sub(r'(\})\n', r'\1', code)
-
-    # remove invalid selectors
-    code = re.sub(r',[\d\s\.\#\+>:]*\{', '{', code)
-    # remove superfluous ;
-    code = re.sub(r';\s*;', ';', code)
-    # add space before and after comment content
-    code = re.sub(r'\/\*\s*([\s\S]+?)\s*\*\/', r'/* \1 */', code)
-    # fix space after http[s]:
-    code = re.sub(r'(http[s]?:) \/\/', r'\1//', code)
-    # add space before !important
-    code = re.sub(r'\s*!important', ' !important', code)
+    code = remove_invalid_selectors(code)
+    code = trim_whitespace(code)
+    code = replace_tabs_with_spaces(code)
+    code = replace_nonspace_whitespaces_with_newlines(code)
+    code = remove_whitespace_around_some_special_characters(code)
+    code = remove_whitespace_before_closing_curly_braces(code)
+    # this might break handling of comments:
+    # code = remove_newline_after_closing_curly_braces(code)
+    code = remove_spaces_between_newlines(code)
+    code = add_missing_semicolons(code)
+    code = remove_superfluous_semicolons(code)
 
     return code
 
 
 def apply_LaterPay_style(code):
-    # add space before {
-    code = re.sub(r'(\S)\{', r'\1 {', code)
-    # add \n after @media {
-    code = re.sub(r'((@media|@[\w-]*keyframes)[^\{]+\{)\s*', r'\1\n', code)
-    # add \n after @charset & @import
-    code = re.sub(r'((?:@charset|@import)[^;]+;)\s*', r'\1\n', code)
-    # add \n before included selector
-    code = re.sub(r';\s*([^\};]+?\{)', r';\n\1', code)
-    # remove space after {
-    code = re.sub(r'{(\s)(\S)', r'{\2', code)
-    # remove space before }
-    code = re.sub(r'(\s)\}', r'\1}', code)
-    # add missing ; before }
-    code = re.sub(r'([^;])\}', r'\1;}', code)
-    # add \n after }
-    code = re.sub(r'\}', r'}\n', code)
-    # preserve up to two consecutive empty lines
-    code = re.sub(r'\n{3,}', r'\n\n\n', code)
+    code = add_space_around_selector_characters(code)
 
-    # remove space after :
-    code = re.sub(r'\s*([\:])\s*', r'\1', code)
-    # add space after ; except for before }
-    code = re.sub(r'(\S);([^\}])', r'\1; \2', code)
-    # replace " with '
-    code = code.replace('"', '\'')
-    # remove ' from within url()
-    code = re.sub(r'url\(\'([^\)]+)\'\)', r'url(\1)', code)
-    # add space before > +
-    code = re.sub(r'(\S)([>+])', r'\1 \2', code)
-    # add space after > +
-    code = re.sub(r'([>+])(\S)', r'\1 \2', code)
-    # remove 0 from 0.x values directly after :
-    code = re.sub(r':0\.', r':.', code)
-    # add space or \n after ,
-    code = comma_rules(code)
+    code = add_space_before_opening_curly_braces(code)
+    code = remove_space_after_opening_curly_braces(code)
+    code = add_newline_at_beginning_of_media_ruleset(code)
+    code = add_newline_after_charset_and_import(code)
+    code = add_newline_before_included_selectors(code)
 
-    # code = re.sub(r'\}\s*(\/\*[\s\S]+?\*\/)\s*', r'}\n\1\n', code)
-    # # add \n before and after outside comment
-    # fix comment after ;
-    code = re.sub(r'\;\s*(\/\*[^\n]*\*\/)\s*', r'; \1\n', code)
-    # remove \n between comment and }
-    code = re.sub(r'(\/\*[^\n]*\*\/)\s+\}', r'\1}', code)
+    code = remove_whitespace_before_closing_curly_braces(code)
+    code = add_newline_after_closing_curly_braces(code)
 
+    code = remove_excess_newlines(code)
+
+    code = add_space_after_semicolon(code)
+    code = remove_space_after_colon(code)
+    code = handle_whitespace_after_comma(code)
+
+    code = replace_double_quotes_with_single_quotes(code)
+    code = remove_quotes_within_urls(code)
+
+    code = fix_space_after_http(code)
     code = fix_data_uris(code)
+
+    code = strip_leading_zeros_from_values(code)
     code = fix_0_values(code)
+
+    code = add_space_before_important(code)
+
     code = format_hex_colors(code)
+
+    code = handle_comments(code)
+
     code = sort_properties(code)
     code = expand_long_rules(code)
     code = indent_rules(code)
 
-    # make sure the file ends with a newline
-    code = code.strip() + '\n'
+    code = add_newline_at_end_of_file(code)
 
     return code
 
 
-def comma_rules(code):
+def trim_whitespace(code):
+    code = code.strip()
+
+    return code
+
+
+def replace_tabs_with_spaces(code):
+    code = re.sub(r'\t', '    ', code)
+
+    return code
+
+
+def replace_nonspace_whitespaces_with_newlines(code):
+    code = re.sub(r'[\r\f\v]', '\n', code)
+
+    return code
+
+
+def remove_newline_after_closing_curly_braces(code):
+    code = re.sub(r'(\})\n', r'\1', code)
+
+    return code
+
+
+def add_newline_after_closing_curly_braces(code):
+    code = re.sub(r'\}', r'}\n', code)
+
+    return code
+
+
+def add_space_before_opening_curly_braces(code):
+    code = re.sub(r'(\S)\{', r'\1 {', code)
+
+    return code
+
+
+def remove_space_after_opening_curly_braces(code):
+    code = re.sub(r'{(\s)(\S)', r'{\2', code)
+
+    return code
+
+
+def remove_whitespace_before_closing_curly_braces(code):
+    code = re.sub(r'\s*(\})', r'\1', code)
+
+    return code
+
+
+def remove_whitespace_around_some_special_characters(code):
+    # remove \s before and after {:;,
+    code = re.sub(r'\s*([\{:;,])\s*', r'\1', code)
+
+    return code
+
+
+def remove_spaces_between_newlines(code):
+    code = re.sub(r'\n[ ]+\n', r'\n\n', code)
+
+    return code
+
+
+def remove_excess_newlines(code):
+    # preserve up to 2 consecutive empty lines
+    code = re.sub(r'\n{3,}', r'\n\n\n', code)
+
+    return code
+
+
+def remove_space_after_colon(code):
+    code = re.sub(r'\s*([\:])\s*', r'\1', code)
+
+    return code
+
+
+def add_space_after_semicolon(code):
+    # add space after ; except for before }
+    code = re.sub(r'(\S);([^\}])', r'\1; \2', code)
+
+    return code
+
+
+def replace_double_quotes_with_single_quotes(code):
+    code = code.replace('"', '\'')
+
+    return code
+
+
+def remove_invalid_selectors(code):
+    code = re.sub(r',[\d\s\.\#\+>:]*\{', '{', code)
+
+    return code
+
+
+def remove_quotes_within_urls(code):
+    code = re.sub(r'url\(\'([^\)]+)\'\)', r'url(\1)', code)
+
+    return code
+
+
+def handle_whitespace_after_comma(code):
+    # add space or \n after ,
     block = code.split('}')
 
     for i in range(len(block)):
@@ -127,10 +199,35 @@ def comma_rules(code):
                     # add space after properties' or @media's ,
                     b[j] = re.sub(r',(\S)', r', \1', b[j])
                 else:
-                    # add \n after selectors' ,
+                    # add \n between multiple selectors after ,
                     b[j] = re.sub(r',(\S)', r',\n\1', b[j])
         block[i] = '{'.join(b)
     code = '}'.join(block)
+
+    return code
+
+
+def remove_superfluous_semicolons(code):
+    code = re.sub(r';\s*;', ';', code)
+
+    return code
+
+
+def add_missing_semicolons(code):
+    code = re.sub(r'([^;])\}', r'\1;}', code)
+
+    return code
+
+
+def handle_comments(code):
+    # add space before and after comment content
+    code = re.sub(r'\/\*\s*([\s\S]+?)\s*\*\/', r'/* \1 */', code)
+    # code = re.sub(r'\}\s*(\/\*[\s\S]+?\*\/)\s*', r'}\n\1\n', code)
+    # add \n before and after outside comment
+    # fix comment after ;
+    code = re.sub(r'\;\s*(\/\*[^\n]*\*\/)\s*', r'; \1\n', code)
+    # remove \n between comment and }
+    # code = re.sub(r'(\/\*[^\n]*\*\/)\s+\}', r'\1}', code)
 
     return code
 
@@ -155,8 +252,10 @@ def sort_properties(code):
             properties = re.sub(r';\s', r';', rule).split(';')
             properties = filter(None, properties)
 
-            # custom sorting required in order to not change the sequence of properties in a way that affects appearance
+            # custom sorting required in order to not chanage the sequence
+            # of properties in a way that affects appearance
             # : must be sorted higher than -
+            # TODO: make sure e.g. border-width: is sorted after border:
             properties = sorted(properties, key=lambda x: x.replace(':', 'a'))
 
             # reconstruct rule
@@ -206,14 +305,34 @@ def indent_rules(code):
     return code
 
 
+def add_space_around_selector_characters(code):
+    code = re.sub(r'(\S)([>+])', r'\1 \2', code)  # add space before > +
+    code = re.sub(r'([>+])(\S)', r'\1 \2', code)  # add space after > +
+
+    return code
+
+
+def fix_space_after_http(code):
+    code = re.sub(r'(http[s]?:) \/\/', r'\1//', code)
+
+    return code
+
+
 def fix_data_uris(code):
-    # remove \s inserted into data URIs by other formatting
+    # TODO: remove \s inserted into data URIs by other formatting
+
+    return code
+
+
+def strip_leading_zeros_from_values(code):
+    # remove 0 from 0.x values directly after colon
+    code = re.sub(r':0\.', r':.', code)
 
     return code
 
 
 def fix_0_values(code):
-    # remove unit from 0 values after \s or :
+    # remove unit from 0 values after \s or colon
     code = re.sub(r'([\s:])0[emprx%]+', r'\g<1>0', code)
 
     return code
@@ -224,10 +343,42 @@ def format_hex_colors(code):
 
     for hexValue in hexColor.finditer(code):
         # lowercase hex colors
+        # TODO: make sure ID selectors are not processed
         hexValueNew = hexValue.group(1).lower()
         # if len(hexValueNew) == 6
         # hexValueNew = re.sub(r'(\w)\1(.)\2(.)\3', r'\1\2\3', hexValueNew) #
         # compress 6-digit values to 3-digit values
         code = code.replace(hexValue.group(1), hexValueNew)
+
+    return code
+
+
+def add_space_before_important(code):
+    code = re.sub(r'\s*!important', ' !important', code)
+
+    return code
+
+
+def add_newline_before_included_selectors(code):
+    code = re.sub(r';\s*([^\};]+?\{)', r';\n\1', code)
+
+    return code
+
+
+def add_newline_at_beginning_of_media_ruleset(code):
+    code = re.sub(r'((@media|@[\w-]*keyframes)[^\{]+\{)\s*', r'\1\n', code)
+
+    return code
+
+
+def add_newline_after_charset_and_import(code):
+    code = re.sub(r'((?:@charset|@import)[^;]+;)\s*', r'\1\n', code)
+
+    return code
+
+
+def add_newline_at_end_of_file(code):
+    code = trim_whitespace(code)
+    code = code + '\n'
 
     return code
